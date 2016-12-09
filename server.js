9ef0c9850d5b681209cfdb771b8a61e58a206f77
Jackson Mac <Jackson Mac>
2016-12-09 14:05:57 -0600

    change port to dynamic env var to attempt fix and log

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var methodOverride = require("method-override");
 
 var app = express();
 var port = process.env.port;
+console.log(port);
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
